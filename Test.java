8719afada6535fc6eba9f75b546b538f2bf18087
Sarfaraz Ahamed Shovon <142936265+Shovon021@users.noreply.github.com>
2025-07-17 23:21:40 +0600

    Update Test.java

--- Test.java
+++ Test.java
@@ -1,5 +1,9 @@
-public class HelloWorld {
+public class EvenOdd {
     public static void main(String[] args) {
-        System.out.println("Hello, World! This is Shovon ");
+        int num = 7;
+        if (num % 2 == 0)
+            System.out.println("Even");
+        else
+            System.out.println("Odd");
     }
 }
